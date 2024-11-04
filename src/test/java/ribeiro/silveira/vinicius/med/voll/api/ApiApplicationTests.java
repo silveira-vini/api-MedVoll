b2673494c9c9a55c45a4c9fc8b371e117b813573
Vinicius Silveira <vsribeiro87@gmail.com>
2024-11-04 11:46:11 -0300

    Initial commit

---
+++ src/test/java/ribeiro/silveira/vinicius/med/voll/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ribeiro.silveira.vinicius.med.voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
