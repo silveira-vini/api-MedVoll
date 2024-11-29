363119869f0cec36a5286d368f7e1c3b4f1a5362
Vinicius Silveira <vsribeiro87@gmail.com>
2024-11-29 16:18:35 -0300

    business rules MedVoll_API

--- src/main/java/ribeiro/silveira/vinicius/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/ribeiro/silveira/vinicius/med/voll/api/infra/security/SecurityConfigurations.java
@@ -30,6 +30,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(req ->
                 {
                     req.requestMatchers("/login").permitAll();
+                    req.requestMatchers("/v3/api-docs", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter,UsernamePasswordAuthenticationFilter.class)
